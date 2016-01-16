b28de9ac1254689435e45366893bef16ce927fd5
Niki <niki.dutskinov@hotmail.com>
2016-01-16 13:45:50 +0200

    #test #fix #bug #AddChips

---
+++ Poker/AddChipsWhenLost.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Poker
+{
+    partial class AddChipsWhenLost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.addChipsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.addChipsTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(48, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "You ran out of chips !";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // addChipsButton
+            // 
+            this.addChipsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addChipsButton.Location = new System.Drawing.Point(12, 226);
+            this.addChipsButton.Name = "addChipsButton";
+            this.addChipsButton.Size = new System.Drawing.Size(75, 23);
+            this.addChipsButton.TabIndex = 1;
+            this.addChipsButton.Text = "Add Chips";
+            this.addChipsButton.UseVisualStyleBackColor = true;
+            this.addChipsButton.Click += new System.EventHandler(this.AddButton);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Location = new System.Drawing.Point(197, 226);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.QuitButton);
+            // 
+            // addChipsTextBox
+            // 
+            this.addChipsTextBox.Location = new System.Drawing.Point(91, 229);
+            this.addChipsTextBox.Name = "addChipsTextBox";
+            this.addChipsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.addChipsTextBox.TabIndex = 3;
+            // 
+            // AddChipsWhenLost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.addChipsTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.addChipsButton);
+            this.Controls.Add(this.label1);
+            this.Name = "AddChipsWhenLost";
+            this.Text = "You Ran Out Of Chips";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addChipsButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox addChipsTextBox;
+    }
+}
---
+++ Poker/AddChipsWhenLost.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poker
+{
+/// <summary>
+/// Form that shows up only when we've lost our chips.
+/// </summary>
+    public partial class AddChipsWhenLost : Form
+    {
+        #region fields
+        private int addChipsValue;
+        private const int MaximumChipsCanBeAdded = 100000000;
+        #endregion
+
+        #region constructor
+        public AddChipsWhenLost()
+        {
+            FontFamily fontFamily = new FontFamily("Arial");
+            InitializeComponent();
+            ControlBox = false;
+            label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        }
+        #endregion
+
+        #region properties
+        public int AddChipsValue
+        {
+            get { return this.addChipsValue; }
+            set { this.addChipsValue = value; }
+        }
+        #endregion
+
+        #region event button methods
+        /// <summary>
+        /// Event that listens if the add button is clicked on the window when we lose all our chips.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void AddButton(object sender, EventArgs e)
+        {
+            int parsedValue;
+
+            // If the value we try to add is more than the maximum
+            if (int.Parse(addChipsTextBox.Text) > MaximumChipsCanBeAdded)
+            {
+                MessageBox.Show("The maximium chips you can add is 100000000");
+                return;
+            }
+            //If the value we try to add is not a number
+            if (!int.TryParse(addChipsTextBox.Text, out parsedValue))
+            {
+                MessageBox.Show("This is a number only field");
+                return;
+
+            }
+            // Add the value
+            else if (int.TryParse(addChipsTextBox.Text, out parsedValue) && int.Parse(addChipsTextBox.Text) <= MaximumChipsCanBeAdded)
+            {
+                this.AddChipsValue = int.Parse(addChipsTextBox.Text);
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// Event that listens if the quit button is clicked on the window when we lose all our chips.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void QuitButton(object sender, EventArgs e)
+        {
+            var message = "Are you sure?";
+            var title = "Quit";
+            var result = MessageBox.Show(
+            message, title,
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question);
+            switch (result)
+            {
+                case DialogResult.No:
+                    break;
+                case DialogResult.Yes:
+                    Application.Exit();
+                    break;
+            }
+        }
+        #endregion
+    }
+}
